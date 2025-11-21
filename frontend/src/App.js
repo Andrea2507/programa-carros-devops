295aa57568324df4bf7f07e72f87f86d8efbdc01
Andrea2507 <andrealh2507@umes.edu.gt>
2025-11-20 22:06:27 -0600

    funcionamiento actualizacion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,7 +33,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>  Carros</h1>
+        <h1>  App carros</h1>
 
         {/* Tabla de carros */}
         <TablaCarros
